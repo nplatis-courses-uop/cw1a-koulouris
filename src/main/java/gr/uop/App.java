67db9ae04f3a55c731517f9b66c5d208e32c3e53
Vasilis Koulouris <basileskouloures@gmail.com>
2023-04-02 09:40:08 +0300

    created buttons list

--- src/main/java/gr/uop/App.java
+++ src/main/java/gr/uop/App.java
@@ -1,9 +1,16 @@
 package gr.uop;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -13,9 +20,31 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var label = new Label("Hello, JavaFX");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        
+        
+        Button ok = new Button("OK");
+        Button cancel = new Button("Cancel");
+        Button help = new Button("Help");
+        VBox buttons = new VBox();
+        buttons.getChildren().add(help);
+        buttons.getChildren().add(cancel);
+        buttons.getChildren().add(ok);
+        VBox.setVgrow(ok, Priority.ALWAYS);
+        VBox.setVgrow(help, Priority.ALWAYS);
+        VBox.setVgrow(cancel, Priority.ALWAYS);
+        buttons.setPadding(new Insets(20));
+
+        HBox center = new HBox();
+        center.getChildren().add(buttons);
+        BorderPane main = new BorderPane();
+        Label status = new Label("Status");
+        main.setBottom(status);
+        main.setCenter(center);
+
+
+        var scene = new Scene(main, 500, 180);
         stage.setScene(scene);
+        stage.setTitle("Settings");
         stage.show();
     }
 
